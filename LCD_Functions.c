b8a1e9ada5fc73a5344d320dde437604faee2e25
Omar Gamal <84602727+OmarGamal85@users.noreply.github.com>
2021-06-04 02:04:58 +0200

    Update LCD_Functions.c

--- LCD_Functions.c
+++ LCD_Functions.c
@@ -1,42 +1,4 @@
-#include "tm4c123gh6pm.h"
-#include <stdint.h>
 
-
-void LCD_init();
-void LCD_command(char command);
-void LCD_data(char data);
-void LCD_clear();
-void LCD_set_Cursor(int line, int block);
-void LCD_delay(int time);
-
-
-
-/* initializes the ports A for commands (3 pins), B for data (8 pins)*/
-void LCD_init(){
-    // Rs   -> A5
-    // Rw   -> A6
-    // E    -> A7
-    // Data -> (B0 - B7)
-
-    // Clock
-    SYSCTL_RCGCGPIO_R = 0X03;               // 0000 0011
-    while(!(SYSCTL_PRGPIO_R & 0X03));
-
-    // Direction and Digital
-    GPIO_PORTA_DIR_R = 0XE0;                // 1110 0000 (Rs, Rw, E)
-    GPIO_PORTA_DEN_R = 0XE0;                 
-    GPIO_PORTB_DIR_R = 0XFF;                // 1111 1111
-    GPIO_PORTB_DEN_R = 0XFF;
-
-    // Use 8 bits
-    LCD_command(0X38);
-    
-    // Clear the LCD
-    LCD_clear();
-
-    // Return home
-    LCD_command(0X02);
-}
 
 /* Takes the hexacode of the command */
 void LCD_command(char command){
